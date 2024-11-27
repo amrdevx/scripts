5cf4c6a34e2322c563e247241e67123392844654
me33awy <me33awy@gmail.com>
2024-11-27 08:56:43 +0200

    Nov 27 (Wed) 08:55:AM

--- log.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CHOOSE=$(printf "POWEROFF\nREBOOT\nZZZ"| dmenu -i -p "choose one")
-
-case $CHOOSE in
-	POWEROFF) doas shutdown -h now ;;
-	REBOOT) doas reboot ;;
-	ZZZ) doas zzz ;;
-esac > /dev/null
